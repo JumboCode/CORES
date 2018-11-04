1e9e82aa2274edd8fee6ee6d4c8a5dbbbde80063
cmurph11 <colinmurphy@wr-130-64-218-182.medford.tufts.edu>
2018-11-04 16:33:11 -0500

    updated title in header.html

--- resources.php
+++ resources.php
@@ -2,7 +2,6 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
 <head>
-<title>Home page – My Website</title>
 <meta http-equiv="description" content="page description" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
