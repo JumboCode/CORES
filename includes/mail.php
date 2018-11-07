728640ca4f206f986401d3bf94e6fad8afa6cba7
Meg <margaret.farley@tufts.edu>
2018-11-06 21:40:09 -0500

    calendar added

--- includes/mail.php
+++ includes/mail.php
@@ -1,9 +1,13 @@
-
-
 <?php 
 if(isset($_POST['submit'])){
-    $to = "aidanfike@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the senders Email address
+    $to = "margaret.farley@tufts.edu"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    if(!filter_var($to, FILTER_VALIDATE_EMAIL))
+    {
+        echo("enter the correct email")}
+    else{
+        $from = 
+    }
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $subject = "Form submission";
@@ -15,8 +19,7 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    header('Location: thank_you.php');
     }
 ?>
